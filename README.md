60f4b092631a3e647518bad4d52b41e469f60046
Peter Tseng <pht24@cornell.edu>
2020-01-03 10:40:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Advent of Code
+
+These are my solutions to http://adventofcode.com
+
+All solutions are written in Rust.
+
+## Input
+
+In general, all solutions can be invoked in both of the following ways:
+
+* Without command-line arguments, takes input on standard input.
+* With 1+ command-line arguments, reads input from the first, which must be the path to an input file.
+  Arguments beyond the first are ignored.
+
+Some may additionally support other ways:
+
+* None yet.
+
+## Closing Thoughts
+
+None yet.
---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+use std::env;
+use std::fs;
+
+#[macro_export]
+macro_rules! tests {
+    (
+        $($test_func:ident {
+            $( $test_name:ident( $( $param:expr ),* ); )+
+        })+
+    ) => {
+        $(
+            $(
+                #[test]
+                fn $test_name() {
+                    $test_func($( $param ),* )
+                }
+            )+
+        )+
+    }
+}
+
+pub fn numbers<T>(s: &str) -> Vec<T>
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    s.split(|c: char| c != '-' && !c.is_ascii_digit())
+        .filter_map(|d| {
+            if d.is_empty() {
+                None
+            } else {
+                Some(
+                    d.parse::<T>()
+                        .expect("numbers should only be called on strings containing numbers"),
+                )
+            }
+        })
+        .collect()
+}
+
+pub fn read_input_lines<T, F>(f: F) -> Vec<T>
+where
+    F: FnMut(&str) -> T,
+{
+    read_input_file().lines().map(f).collect()
+}
+
+pub fn read_input_file() -> String {
+    let filename = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "/dev/stdin".to_string());
+    fs::read_to_string(filename).expect("caller should provide a valid filename in ARGV or stdin")
+}
